26e2e3a42530b78fc0b4eb71df8eb0070c1c8125
bioburn <jckylau@yahoo.com>
2016-01-13 21:41:16 +0000

    test course

---
+++ spec/models/course.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Course, :type => :model do
+	let(:course_instance) { Course.create(dept: "Math", num: "3A", desc: "Calculus") }
+	describe "public instance methods" do
+	    context "responds to its methods" do
+	    	it { expect(course_instance).to respond_to(:course_num) }
+	    end
+	end
+end
