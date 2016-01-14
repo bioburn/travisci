a02306c6ee0eeb23f6eb02436ba336465518a19e
bioburn <jckylau@yahoo.com>
2016-01-14 01:38:08 +0000

    i wrote the wrong test, fixed test

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Course, :type => :model do
     let(:course_instance) { Course.create(dept: "Swordfighting", num: "77", desc: "swordfighting for noobs") }
     describe "public instance methods" do
        context "result equals expected" do
-            it { expect(course_instance.course_num).to eq("swordfighting 77 swordfighting for noobs") }  
+            it { expect(course_instance.course_num).to eq("swordfighting 77") }  
        end
     end
     
